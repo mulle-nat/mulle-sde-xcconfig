0cb7547c1e4e935a8f86a94def6baf7854799796
Nat! <nat@mulle-kybernetik.com>
2016-04-13 15:34:05 +0200

    fix

--- Common.cmake
+++ Common.cmake
@@ -1,11 +1,5 @@
-set( DEPENDENCY_FRAMEWORK_DIRS
-dependencies/Frameworks/Release
-dependencies/Frameworks
-)
-
-set( DEPENDENCY_LIBRARY_DIRS
-dependencies/lib/Release
-dependencies/lib
+include_directories( BEFORE SYSTEM
+dependencies/include
 )
 
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.4" CACHE STRING "Deployment target for OSX" FORCE)
