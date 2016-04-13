76d741fda8ac836e36a4a789e1cc861a28318016
Nat! <nat@mulle-kybernetik.com>
2016-04-13 15:41:49 +0200

    change to 10.6 for cmake and rpath

--- Common.cmake
+++ Common.cmake
@@ -2,4 +2,5 @@ include_directories( BEFORE SYSTEM
 dependencies/include
 )
 
-set(CMAKE_OSX_DEPLOYMENT_TARGET "10.4" CACHE STRING "Deployment target for OSX" FORCE)
+# baseline set to 10.6 for rpath
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.6" CACHE STRING "Deployment target for OSX" FORCE)
