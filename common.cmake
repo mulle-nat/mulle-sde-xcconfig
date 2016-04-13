92ed0413a9dbc82139b3cc8bb660c5197d65de67
Nat! <nat@mulle-kybernetik.com>
2016-04-13 15:14:20 +0200

    cmake progress

---
+++ common.cmake
@@ -0,0 +1,11 @@
+set( DEPENDENCY_FRAMEWORK_DIRS
+dependencies/Frameworks/Release
+dependencies/Frameworks
+)
+
+set( DEPENDENCY_LIBRARY_DIRS
+dependencies/lib/Release
+dependencies/lib
+)
+
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.4" CACHE STRING "Deployment target for OSX" FORCE)
--- staticlibrary.cmake
+++ staticlibrary.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_OSX_DEPLOYMENT_TARGET "10.4" CACHE STRING "Deployment target for OSX" FORCE)
+include( common)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -g -DNDEBUG -Wno-parentheses")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -g -DNDEBUG -Wno-parentheses")
