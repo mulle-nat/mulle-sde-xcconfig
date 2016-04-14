fe2b08fb75f6a097726522f9b5901f4aa406a004
Nat! <nat@codeon.de>
2016-04-14 14:31:56 +0200

    different handling, depending on being executed by mulle-bootstrap or not

--- Common.cmake
+++ Common.cmake
@@ -1,6 +1,22 @@
-include_directories( BEFORE SYSTEM
-dependencies/include
-)
+# Just for C-Projects, use different branch for ObjC
 
 # baseline set to 10.6 for rpath
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.6" CACHE STRING "Deployment target for OSX" FORCE)
+
+#
+# if using mulle_bootstrap, DEPENDENCIES_DIR  is defined and
+# mulle-boostrap will set up the paths, so don't mess with it
+#
+# These setting are for ppl. who build the project as a top
+# level project
+#
+if( NOT DEPENDENCIES_DIR )
+   set( DEPENDENCY_LIBRARY_DIRS
+   dependencies/lib/Release
+   dependencies/lib
+   )
+
+   include_directories( BEFORE SYSTEM
+      dependencies/include
+   )
+endif()
