6224d28a7fdebed7eea6b0c5cd9d7e8b87b2c4c6
Nat! <nat@codeon.de>
2016-08-28 17:53:10 +0200

    remove some stuff that makes unexpected problems. turn off unknown #pragma warnings

--- Common.cmake
+++ Common.cmake
@@ -4,12 +4,17 @@
 #
 include( _Common)
 
-
 if( (CMAKE_C_COMPILER_ID STREQUAL "Clang") OR
     (CMAKE_C_COMPILER_ID STREQUAL "AppleClang") OR
     (CMAKE_C_COMPILER_ID STREQUAL "MulleClang") OR
     (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
    set( UNWANTED_C_WARNINGS "-Wno-parentheses -Wno-int-to-void-pointer-cast")
+else
+	if((CMAKE_C_COMPILER_ID STREQUAL "Intel") OR
+    (CMAKE_C_COMPILER_ID STREQUAL "MSVC"))
+    	# C4068: unwanted pragma
+      set( UNWANTED_C_WARNINGS "/wd4068")
+	endif()
 endif()
 
 message(STATUS "CMAKE_C_COMPILER_ID is ${CMAKE_C_COMPILER_ID}")
