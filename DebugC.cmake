190ce7f79dc2a889b6386bb14d6c9790acf0b980
Nat! <nat@codeon.de>
2016-10-17 14:43:19 +0200

    improve win32

--- DebugC.cmake
+++ DebugC.cmake
@@ -13,7 +13,7 @@ set( COMMON_LIBRARY_DIRS
 #
 
 if( MULLE_C_COMPILER_ID MATCHES "^(Intel|MSVC|MSVC-Clang|MSVC-MulleClang)$")
-   set( OTHER_C_FLAGS "${OTHER_C_FLAGS} /Od /Z7 /DDEBUG=1 ")
+   set( OTHER_C_FLAGS "${OTHER_C_FLAGS} /Od /DEBUG /Zi /DDEBUG=1 ")
 else()
    set( OTHER_C_FLAGS "${OTHER_C_FLAGS} -O0 -g -DDEBUG")
 endif()
